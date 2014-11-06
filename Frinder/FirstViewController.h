6a0d4ce7815a0659edacfeab1ac8a4faa3db663a
Jeanie House <jeaniehouse@jeanies-mbp-2.attlocal.net>
2014-11-06 11:57:05 -0500

    Initial Commit

---
+++ Frinder/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Frinder
+//
+//  Created by Jeanie House on 11/6/14.
+//  Copyright (c) 2014 Jeanie House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ Frinder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Frinder
+//
+//  Created by Jeanie House on 11/6/14.
+//  Copyright (c) 2014 Jeanie House. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
